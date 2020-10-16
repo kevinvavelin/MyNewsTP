0b4d44bc38d8db1a14d818870513c34608d605fc
Kevin Vavelin <kvavelin@geek-inc.fr>
2020-10-16 12:28:39 +0200

    Initial Commit

---
+++ MyNews/MyNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewsApp.swift
+//  MyNews
+//
+//  Created by Kevin Vavelin on 16/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct MyNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
